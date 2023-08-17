94951b8a7346e1399cc09dbf999ea9d3f2a1ff9a
ELEGANCIARESPETOYMALDAD <kb3270794@gmail.com>
2023-08-16 22:35:19 -0500

    proy_02

---
+++ app.js
@@ -0,0 +1,8 @@
+const functions = require('./functions.js')
+
+console.log(functions);
+
+console.log(functions.add(1,5));
+console.log(functions.substract(1,8));
+console.log(functions.multiply(1,8));
+console.log(functions.divide(0,5));
